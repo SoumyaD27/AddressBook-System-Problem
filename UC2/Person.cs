0278035c81b1cc00c10f357ae81d1efdfa038434
SoumyaD27 <soumyad233@gmail.com>
2022-12-08 14:11:09 +0530

    Ability to add a new Contact to Address Book.

---
+++ UC2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2
+{
+    public class Person
+    {
+        public static void CreateContacts()
+        {
+            Contacts contacts = new Contacts();
+
+            Console.WriteLine("Enter First Name: ");
+            contacts.firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            contacts.lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter City Name: ");
+            contacts.city = Console.ReadLine();
+
+            Console.WriteLine("Enter State Name: ");
+            contacts.state = Console.ReadLine();
+
+            Console.WriteLine("Enter zip Code: ");
+            contacts.zipCode = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number: ");
+            contacts.phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email ID: ");
+            contacts.emailID = Console.ReadLine();
+
+            Program.Person.Add(contacts);
+        }
+    }
+}    
---
+++ UC2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace UC2
+{
+    public class Program
+    {
+        public static List<Contacts> Person = new List<Contacts>();
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Address Book");
+            UC2.Person.CreateContacts();
+        }
+    }
+}
