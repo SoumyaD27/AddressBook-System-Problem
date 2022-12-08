e1be0be9354947cba56c1e52a43a72bbc31b7dd5
SoumyaD27 <soumyad233@gmail.com>
2022-12-08 13:53:46 +0530

    Ability to create Contacts in Address Book with details.

---
+++ UC1/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1
+{
+    public class Person
+    {
+        public static void CreateContracts()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name: ");
+            contacts.firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name: ");
+            contacts.lastName = Console.ReadLine();
+            Console.WriteLine("Enter City Name: ");
+            contacts.city = Console.ReadLine();
+            Console.WriteLine("Enter State Name: ");
+            contacts.state = Console.ReadLine();
+            Console.WriteLine("Enter zip Code: ");
+            contacts.zipCode = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number: ");
+            contacts.phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email ID: ");
+            contacts.emailID = Console.ReadLine();
+        }
+    }
+}
---
+++ UC1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UC1
+{
+    class Program
+    {
+        public static List<Contacts> Person = new List<Contacts>();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
